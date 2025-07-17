1268e226b6e87f1dafbb30ef77dc58f4ef10a13f
adelekekismath <adelekekismath9@gmail.com>
2025-07-17 16:27:24 +0200

    refactor: update user model to include IUser interface and adjust import statement in auth routes

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,7 +1,14 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<IUser>({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true}
@@ -15,8 +22,8 @@ userSchema.pre('save', async function (next) {
 })
 
 
-userSchema.methods.comparePassword = function (candidatePassword: string) {
+userSchema.methods.comparePassword = function (candidatePassword: string): Promise<boolean> {
   return bcrypt.compare(candidatePassword, this.password)
 }
-
-export const User = mongoose.model('User', userSchema)
+const User: mongoose.Model<IUser> = mongoose.model<IUser>('User', userSchema);
+export default User;
